f978c440abc6985eaa67abf9fa335875f5476ae3
Taihsiang Ho (tai271828) <e@tai271828.me>
2022-05-02 13:38:55 +0200

    prototype of the click entry

--- rg_cli.py
+++ rg_cli.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
-def hello():
+import click
+import spreadsheetX
+
+@click.option
+def load_slide():
+    load.open("michelle_slide_template.ppt")
     print("Hello PyCon APAC 2022")
+
+@click.option
+def fill_in_slide_data():
+    fill("asdf.ppt")
 
 if __name__ == '__main__':
     hello()
